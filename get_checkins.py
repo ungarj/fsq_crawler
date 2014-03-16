01e2a50b85376e31901faa383917a4aaa154ffd0
Joachim Ungar <joachim.ungar@eox.at>
2014-03-16 21:15:22 +0100

    spatialite insert

--- get_checkins.py
+++ get_checkins.py
@@ -2,8 +2,16 @@
 
 import foursquare
 import os
+from pyspatialite import dbapi2 as db
+
 
 def main():
+
+    # creating/connecting the test_db
+    conn = db.connect('venues.sqlite')
+    # creating a Cursor
+    cur = conn.cursor()
+
     # read env variables
     client_id = os.environ['CLIENT_ID']
     client_secret = os.environ['CLIENT_SECRET']
@@ -19,32 +27,39 @@ def main():
     	name = venue['name']
     	#category_name venue['categories']
     	#category_parents venue['categories']
-    	address = venue['location']['address']
+    	#address = venue['location']['address']
     	lat = venue['location']['lat']
     	lon = venue['location']['lng']
-    	state = venue['location']['state']
-        postal_code = venue['location']['postalCode']
+    	#state = venue['location']['state']
+        #postal_code = venue['location']['postalCode']
     	checkins_count = venue['stats']['checkinsCount']
     	users_count = venue['stats']['usersCount']
     	verified = venue['verified']
     	here_now = venue['hereNow']['count']
 
-        print name
-    	print address
-    	print lat
-    	print lon
-    	print state
-    	print postal_code
-    	print checkins_count
-    	print users_count
-    	print verified
-    	print here_now
+        #print name
+    	#print lat
+    	#print lon
+    	#print checkins_count
+    	#print users_count
+    	#print verified
+    	#print here_now
+
+        insert_venue = 'INSERT INTO fsq_venues (fsq_id, name, Geometry) VALUES ("%s","%s", GeomFromText("POINT(%s %s)"));' %(fsq_id, name, lon, lat)
+        print insert_venue
+        cur.execute(insert_venue)
+        conn.commit()
 
     # TODO: get high level category
     #category_list = client.venues.categories()
     #for category in category_list:
     # 	print category['name']
 
+    # testing library versions
+    #rs = cur.execute('SELECT sqlite_version(), spatialite_version()')
+    #for row in rs:
+    #    msg = "> SQLite v%s Spatialite v%s" % (row[0], row[1])
+    #    print msg
     
 if __name__ == "__main__":
     main()
--- init_spatialite.sh
+++ init_spatialite.sh
@@ -1,23 +1,27 @@
 spatialite venues.sqlite "SELECT InitSpatialMetaData();"
 
-spatialite venues.sqlite "CREATE TABLE fsq_venues
-( PKUID INTEGER PRIMARY KEY AUTOINCREMENT ,
-  fsq_id           TEXT,
-  name             TEXT,
-  CategoryName     TEXT,
-  CategoryParents  TEXT,
-  Address          TEXT,
-  city             TEXT,
-  lat             FLOAT,
-  lon             FLOAT,
-  state            TEXT,
-  postalCode        INT,
-  checkinsCount     INT,
-  usersCount        int,
-  verified         TEXT
-);"
+spatialite venues.sqlite 'CREATE TABLE fsq_venues (
+PK_UID INTEGER PRIMARY KEY AUTOINCREMENT,
+timestamp TEXT,
+request_id TEXT,
+herenow INTEGER,
+fsq_id TEXT,
+id INTEGER,
+name TEXT,
+categoryna TEXT,
+categorypa TEXT,
+address TEXT,
+city TEXT,
+lat DOUBLE,
+lon DOUBLE,
+state TEXT,
+postalcode TEXT,
+checkinsco INTEGER,
+userscount INTEGER,
+verified TEXT,
+Geometry BLOB NOT NULL);'
 
-spatialite venues.sqlite "SELECT addGeometryColumn('fsq_venues', 'geom', 4326, 'MULTILINESTRING', 2);"
+#spatialite venues.sqlite "SELECT addGeometryColumn('fsq_venues', 'geom', 4326, 'POINT', 2);"
 
 #INSERT INTO fsq_venues (fsq_id, geom, name, CategoryName, CategoryParents, Address, city, lat, lon, state, postalCode, checkinsCount, usersCount, verified)
 #INSERT INTO fsq_requests (fsq_id, hereNow, request_id)
